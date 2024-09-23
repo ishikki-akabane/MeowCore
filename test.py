0d375dfb1e4e5fd9b37dd612dda760ba5cff34c1
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-23 21:28:59 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+
+from MeowCore import MeowCore
+
+
+MeowClient = MeowCore(
+    "69696969-MeowMeow",
+    category="telegram",
+    bot_id="123456789",
+    bot_username="iSHiKKiBot"
+)
+print("hoi")
