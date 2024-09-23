77fa0968e76a58100c9137eea9f3a8af7ec614bd
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-23 23:15:00 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,14 +2,13 @@ import asyncio
 from MeowCore import MeowCore
 
 
-MeowClient = MeowCore(
-    "69696969-MeowMeow",
-    category="telegram",
-    bot_id="123456789",
-    bot_username="iSHiKKiBot"
-)
-
 async def aa():
+    MeowClient = MeowCore(
+        "69696969-MeowMeow",
+        category="telegram",
+        bot_id="123456789",
+        bot_username="iSHiKKiBot"
+    )
     await MeowClient.load_welcome(["x00xhaha"])
 
     print("hoi")
