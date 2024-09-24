071a8ed84a0adb0256fd5baefd8de9d1253d1be8
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-24 14:59:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 import asyncio
 from MeowCore import MeowCore
+import logging
 
+
+FORMAT = "[TEST] %(message)s"
+logging.basicConfig(
+    handlers=[logging.FileHandler("logs.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+    format=FORMAT,
+    datefmt="[%X]",
+)
+
+LOGGER = logging.getLogger('[TEST]')
 
 async def aa():
     MeowClient = MeowCore(
